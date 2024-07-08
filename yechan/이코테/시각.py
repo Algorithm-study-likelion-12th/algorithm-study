34e2a350127ded8b8ba269a9242c6d21b2dd2fc0
yeahcold <roganyc@naver.com>
2024-07-08 18:01:05 +0900

    week2 📒: 시각

---
+++ yechan/이코테/시각.py
@@ -0,0 +1,15 @@
+# 시각
+import sys
+sys.stdin = open("input.txt", "rt")
+
+n = int(input())
+cnt = 0
+
+for i in range(n+1):
+    for j in range(60):
+        for l in range(60):
+            if "3" in str(i)+str(j)+str(l):
+                cnt += 1
+print(cnt)
+
+
