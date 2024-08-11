4a4f07d1cb9c877956e1c57147f23cc328c3c3af
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-08-11 22:34:06 +0900

    week6 🌐: 2110번, 2467번, 3079번 해결

---
+++ myeonggyu/백준/week6/2467.py
@@ -0,0 +1,30 @@
+# 용액
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+data = list(map(int, input().split()))
+
+left, right = 0, 0
+
+diff = 9999999999
+for i in range(N - 1):
+    dt = data[i]
+    start, end = i + 1, N - 1
+    while start <= end:
+        mid = (start + end) // 2
+        temp = dt + data[mid]
+
+        if abs(temp) < diff:
+            diff = abs(temp)
+            left = i
+            right = mid
+            if temp == 0:
+                break
+        if temp < 0:
+            start = mid + 1
+        elif temp > 0:
+            end = mid - 1
+
+print(data[left], data[right])
